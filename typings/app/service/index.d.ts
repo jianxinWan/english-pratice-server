3bea405301323ba336b4489a253b1bde2180bac8
wanjianxin <wanjianxin@meituan.com>
2020-05-06 23:20:16 +0800

    专项演练

--- typings/app/service/index.d.ts
+++ typings/app/service/index.d.ts
@@ -1,7 +1,11 @@
-// This file is created by egg-ts-helper@1.25.6
+// This file is created by egg-ts-helper@1.25.7
 // Do not modify this file!!!!!!!!!
 
 import 'egg';
+type AnyClass = new (...args: any[]) => any;
+type AnyFunc<T = any> = (...args: any[]) => T;
+type CanExportFunc = AnyFunc<Promise<any>> | AnyFunc<IterableIterator<any>>;
+type AutoInstanceType<T, U = T extends CanExportFunc ? T : T extends AnyFunc ? ReturnType<T> : T> = U extends AnyClass ? InstanceType<U> : U;
 import ExportExercise = require('../../../app/service/exercise');
 import ExportHome = require('../../../app/service/home');
 import ExportSpider = require('../../../app/service/spider');
@@ -11,11 +15,11 @@ import ExportUserInfo = require('../../../app/service/user_info');
 
 declare module 'egg' {
   interface IService {
-    exercise: ExportExercise;
-    home: ExportHome;
-    spider: ExportSpider;
-    stem: ExportStem;
-    submit: ExportSubmit;
-    userInfo: ExportUserInfo;
+    exercise: AutoInstanceType<typeof ExportExercise>;
+    home: AutoInstanceType<typeof ExportHome>;
+    spider: AutoInstanceType<typeof ExportSpider>;
+    stem: AutoInstanceType<typeof ExportStem>;
+    submit: AutoInstanceType<typeof ExportSubmit>;
+    userInfo: AutoInstanceType<typeof ExportUserInfo>;
   }
 }
